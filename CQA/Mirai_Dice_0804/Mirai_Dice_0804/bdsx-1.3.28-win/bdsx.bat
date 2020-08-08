d6153dcc0ea437c8b7c2f920168dd9d89ed4bd75
gxh <46601807+littlegao233@users.noreply.github.com>
2020-08-08 21:42:18 +0800

    .

--- CQA/Mirai_Dice_0804/Mirai_Dice_0804/bdsx-1.3.28-win/bdsx.bat
+++
@@ -1,8 +0,0 @@
-
-@%~dp0\bin\bdsx-cli-win %*
-@if "%errorlevel%" neq "2" goto :eof
-
-@set TARGET="%cd%\bdsx"
-@pushd "%UserProfile%\.bds" 
-@"%UserProfile%\.bds\bedrock_server.exe" "%TARGET%"
-@popd
